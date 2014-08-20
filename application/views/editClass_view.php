044f492eff4c439b44cc1c458c5dac21512b242c
rforeman416 <rachel.ivy.foreman@gmail.com>
2014-08-19 22:06:24 -0400

    one more fluid change

--- application/views/editClass_view.php
+++ application/views/editClass_view.php
@@ -22,42 +22,56 @@
 </head>
 
 <body>
-
+<br>
+<br>
+<div class="container-fluid">
 	<?php echo validation_errors(); ?>
-		
+	<table><td class='noCenter'>
+	
 	<!--Edit Form-->
 	<form class="form-horizontal" role="form" action='<?= base_url();?>index.php/classController/editClass/<?php echo $class->idclasses?>' method="post">
 
 		<!--Title Input-->
-		<label for="title" class="col-lg-3 control-label">Title: </label>
-		<div class="col-lg-6">
-			<input type="text" class="form-control" name="title" id="title"  value="<?php echo $class->title; ?>" size="20">
+		<div class="row">
+			<label for="title" class="col-lg-2 control-label">Title: </label>
+			<div class="col-lg-6">
+				<input type="text" class="form-control" name="title" id="title"  value="<?php echo $class->title; ?>" size="20">
+			</div>
 		</div>
 		<br>
 
-		<!--Teacher Input-->		
-		<label for="teacher" class="col-lg-3 control-label">Teacher: </label>
-		<div class="col-lg-6">
-			<input type="text" class="form-control" name="teacher" id="teacher"  value="<?php echo $class->teacher; ?>" size="20">
+		<!--Teacher Input-->
+		<div class="row">		
+			<label for="teacher" class="col-lg-2 control-label">Teacher: </label>
+			<div class="col-lg-6">
+				<input type="text" class="form-control" name="teacher" id="teacher"  value="<?php echo $class->teacher; ?>" size="20">
+			</div>
 		</div>
 		<br>
 
-		<!--Room Input-->		
-		<label for="room" class="col-lg-3 control-label">Room: </label>
-		<div class="col-lg-6">
-			<input type="text" class="form-control" name="room" id="room"  value="<?php echo $class->room; ?>" size="20">
+		<!--Room Input-->
+		<div class="row">		
+			<label for="room" class="col-lg-2 control-label">Room: </label>
+			<div class="col-lg-6">
+				<input type="text" class="form-control" name="room" id="room"  value="<?php echo $class->room; ?>" size="20">
+			</div>
 		</div>
 		<br>
 
-		<!--Submit Button-->		
-		<div class="col-lg-offset-3 col-lg-10">
-		  <button type="submit" class="btn btn-success">Edit</button> 
+		<!--Submit Button-->
+		<div class="row">
+			<div class="col-lg-offset-6 col-lg-10">
+			  <button type="submit" class="btn btn-success">Edit Class</button> 
+			</div>
 		</div>
 
 	</form>
 	<br>
 	<br>
-
-
+	</td>
+</table>
+<br>
+<br>
+</div>
 </body>
 </html>
